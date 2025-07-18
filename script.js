f2877f2980e2e3a16351cba778be3c85deb6156e
Mohammed Elsammak <elsamak_1@yahoo.com>
2025-07-18 11:26:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
-var map = L.map('map').setView([25.1972, 55.2744], 14);
+// Initialize map centered at Burj Khalifa
+const map = L.map('map').setView([25.1972, 55.2744], 15);
 
+// Tile layer from OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
+  maxZoom: 19,
+  attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var mainBuilding = L.marker([25.1972, 55.2744]).addTo(map)
-    .bindPopup('Burj Khalifa')
-    .openPopup();
+// Add marker for Burj Khalifa
+const burjKhalifa = L.marker([25.1972, 55.2744])
+  .addTo(map)
+  .bindPopup('<b>Burj Khalifa</b><br>World\'s tallest building.')
+  .openPopup();
+
+// Famous POIs
+const pois = [
+  {
+    name: "The Dubai Mall",
+    lat: 25.1984,
+    lng: 55.279,
+    info: "The world's largest shopping mall."
+  },
+  {
+    name: "Dubai Fountain",
+    lat: 25.195,
+    lng: 55.275,
+    info: "The world's largest choreographed fountain system."
+  },
+  {
+    name: "Dubai Opera",
+    lat: 25.192,
+    lng: 55.275,
+    info: "A modern performing arts center."
+  }
+];
+
+// Routing control (will be updated on marker click)
+let routeControl = L.Routing.control({
+  waypoints: [],
+  routeWhileDragging: false,
+  addWaypoints: false,
+  draggableWaypoints: false,
+  show: false
+}).addTo(map);
+
+// Add POI markers and click handlers
+pois.forEach(poi => {
+  const marker = L.marker([poi.lat, poi.lng]).addTo(map);
+  marker.bindPopup(`<b>${poi.name}</b><br>${poi.info}`);
+  marker.on('click', () => {
+    routeControl.setWaypoints([
+      burjKhalifa.getLatLng(),
+      marker.getLatLng()
+    ]);
+    marker.openPopup();
+  });
+});
